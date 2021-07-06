a78d27648f56b72bc2e3c2d83c34f4088db0fecf
Jonathan Math <jonathanamath@gmail.com>
2021-07-06 08:41:48 -0600

    fix testrunner

--- utils/testrunner.py
+++ utils/testrunner.py
@@ -1,18 +1,20 @@
 from django.test.runner import DiscoverRunner
 from django.test.utils import setup_databases
 from mongoengine.connection import disconnect, connect
+from django.conf import settings
 class TestRunner(DiscoverRunner):
     def setup_databases(self, **kwargs):
-        db_name = 'newsblur_test'
         disconnect()
-        connect(db_name)
-        print('Creating test-database: ' + db_name)
+        mongo_db = settings.MONGO_DB
+        connect(**mongo_db)
+        print('Creating test-database: ' + mongo_db.get('name'))
 
         return setup_databases(self.verbosity, self.interactive, **kwargs)
 
     def teardown_databases(self, old_config, **kwargs):
         import pymongo
-        conn = pymongo.MongoClient()
+        mongo_db = settings.MONGO_DB
+        conn = pymongo.MongoClient(host=mongo_db.get('host'), port=mongo_db.get('port'))
         db_name = 'newsblur_test'
         conn.drop_database(db_name)
         print('Dropping test-database: %s' % db_name)
